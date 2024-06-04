3a9e13331b673ce965b74389e8d58ae9ad29a644
IGN. Brindawan Tri Guna Yoga <im.brindawan@gmail.com>
2024-06-05 01:14:05 +0700

    setup nodemon

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-const data = "testing"
-console.log(data)
+const express = require('express')
+const app = express()
+const port = 3000
+
+// main route / endpoint
+app.get('/', (req, res) => {
+    res.send('Hello Worlds!')
+})
+
+// post
+app.post('/login', (req, res) => {
+    // if (req.name === "user") {
+        res.send('Login success!')
+    // }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
